6b0d333f4689ce7ab0d8d3adb84aa99b1fd5235f
秦志鹏 <qinzhipeng@qinzhipengdebijibendiannao.local>
2023-03-26 15:55:53 +0800

    feat: add alias config

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'father';
 
 export default defineConfig({
-  cjs: { output: 'dist' }
+  cjs: {
+    output: 'dist',
+    alias: {
+      '@/*':'./src/*'
+    }
+  }
 });
