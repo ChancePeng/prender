8e16d01459059a43d6104e03b64d33983dc93425
change <1932583520@qq.com>
2023-03-27 10:19:19 +0800

    fix: remove bug

--- src/render/major/typs.ts
+++ src/render/major/typs.ts
@@ -1,42 +1,41 @@
 import type { ColumnType } from '@/components';
-import type { ReactNode, CSSProperties } from 'react';
+import type { CSSProperties, ReactNode } from 'react';
 import { MiddlewareType } from '../middleware/type';
 import type { ComponentType } from '../type';
 
 interface FieldProps {
-  className?: string,
-  style?: CSSProperties,
-  [data: string]: any
+  className?: string;
+  style?: CSSProperties;
+  [data: string]: any;
 }
 
-export type VisibleType = boolean
+export type VisibleType =
+  | boolean
   | 'hidden-while-empty-origin'
   | 'hidden-while-empty-dataIndex'
-  | (() => boolean)
-
+  | (() => boolean);
 
 export interface IConfig {
-  readonly type: ComponentType,
-  readonly instanceOf: ComponentType,
-  dataIndex?: string | string[],
-  columns?: ColumnType<unknown>[],
-  dataSource?: any,
-  bordered?: boolean,
-  visible?: VisibleType,
-  fieldProps?: FieldProps,
-  children?: IConfig[],
-  readonly __origin?: any,
-  readonly __data?:any,
-  header?: (data?: any, record?: any) => ReactNode | ReactNode,
-  footer?: (data?: any, record?: any) => ReactNode | ReactNode | string[],
-  beforeDataRendered?: (data: any, record: any) => any,
-  renderEmpty?: (record: any) => ReactNode,
-  render?: (data: any, record: any, dom: ReactNode) => ReactNode,
+  readonly type: ComponentType;
+  readonly instanceOf?: ComponentType;
+  dataIndex?: string | string[];
+  columns?: ColumnType<unknown>[];
+  dataSource?: any;
+  bordered?: boolean;
+  visible?: VisibleType;
+  fieldProps?: FieldProps;
+  children?: IConfig[];
+  readonly __origin?: any;
+  readonly __data?: any;
+  header?: (data?: any, record?: any) => ReactNode | ReactNode;
+  footer?: (data?: any, record?: any) => ReactNode | ReactNode | string[];
+  beforeDataRendered?: (data: any, record: any) => any;
+  renderEmpty?: (record: any) => ReactNode;
+  render?: (data: any, record: any, dom: ReactNode) => ReactNode;
 }
 
-
 export interface Options {
-  pfcs?: Record<string, any>,
-  data?: Record<string, any>,
-  middlewares?:MiddlewareType
+  pfcs?: Record<string, any>;
+  data?: Record<string, any>;
+  middlewares?: MiddlewareType;
 }
