5a04bd3bb21e8b396b8e9dd9b201a09cb444b30a
change <qinzhipeng@allqall.com>
2024-08-29 10:34:41 +0800

    feat: update static constant

---
+++ src/components/constant.ts
@@ -0,0 +1,12 @@
+import { ColumnType } from "./type"
+
+type StaticColumnKey = 'INDEX'
+
+export const STATIC_COLUMNS: Record<StaticColumnKey, ColumnType> = {
+  INDEX: {
+    title: '序号',
+    render(_, __, index) {
+      return index ?? 0 + 1
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './components';
 export * from './hooks';
 
 export * from './render';
+
+export * from './constant'
